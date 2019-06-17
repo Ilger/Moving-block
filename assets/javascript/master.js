89e43edff2608666d157ff7e1827803125013ab2
ilger <ilgerdepraetere@gmail.com>
2019-06-17 16:38:31 +0200

    make canvas size = page size, add the variables and consolelog to check

---
+++ assets/javascript/master.js
@@ -0,0 +1,10 @@
+
+
+
+
+let canvas = document.getElementById('canvas')
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+console.log(canvas.height);
+console.log(canvas.width);
