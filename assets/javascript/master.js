045f5bff3bf48001de76cafa1d782905211ca4cf
ilger <ilgerdepraetere@gmail.com>
2019-06-18 09:26:41 +0200

    add comments and set context canvas

--- assets/javascript/master.js
+++ assets/javascript/master.js
@@ -1,10 +1,12 @@
 
 
 
-
-let canvas = document.getElementById('canvas')
+// get canvas element
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
+// log to check everything is good
 console.log(canvas.height);
 console.log(canvas.width);
